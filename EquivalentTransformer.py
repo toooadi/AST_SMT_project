38a54fe649853e7e6db56065f5bdcca7b9f5b8c7
Fabian Epp <fabian@DESKTOP-LM8FBS8>
2024-05-02 14:18:23 +0200

    Add TODOs for later implementation

--- EquivalentTransformer.py
+++ EquivalentTransformer.py
@@ -17,6 +17,13 @@ class EquivalentTransformer:
     def get_applicable_transformations(self, formula):
         return [i for i in allTf.ALL if allTf.all_dict[i].is_applicable(formula)]
     
+    #TODO: (1) We should define some heuristics for the transformation, so that it's not always the  most trivial transformations
+    #          that get executed. One idea is to define some kind of ranking of the transformations, so that the simple ones are
+    #          only executed if there is no other option
+    #      (2) Implement functionality so that the substituter doesn't always go for the first possible match, but maybe the second
+    #          or third one. This is especially important for the trivial transformations (which can always be executed) as they
+    #          would always be executed top-level otherwise. Idea for this: Keep track of the tree depth and only transform on 
+    #          level >=limit OR on the deepest level <limit which is transformable but doesn't have any transformable children
     def transform(self, formula, transformationId=None, generating_formula=None): #last two can be set for testing purposes
         applicable = self.get_applicable_transformations(formula)
         self.substituter.set_transformation(allTf.all_dict[transformationId])
