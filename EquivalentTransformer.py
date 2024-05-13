06f9e50b88361e539bf3121d9e55baabb8e14638
Fabian Epp <fabian@DESKTOP-LM8FBS8>
2024-05-13 17:04:17 +0200

    skeleton for how to check sat

--- EquivalentTransformer.py
+++ EquivalentTransformer.py
@@ -4,11 +4,15 @@ import random
 import os
 from Substituter import FirstOccurenceSubstituter, NDepthSubstituter
 from pysmt.smtlib.parser import SmtLibParser
+import pysmt.smtlib.commands as commands
+from pysmt.logics import QF_LIA, QF_LRA, QF_NIA, LIA
+import subprocess
 
 from pysmt.shortcuts import And, Or, Symbol
 
 class EquivalentTransformer:
-    
+    SAT = "sat"
+    UNSAT = "unsat"
 
     """
     main class to fetch the formulas and perform equivalence transformations on them
@@ -37,19 +41,32 @@ def main():
     #TODO: Implement fetching of formulas from SMT-LIB suite, transformation etc. Implement with parameters like #transformations etc.
     parser = SmtLibParser()
 
-    directory = "benchmarks/<ADD REST OF PATH>"
+    directory = "benchmarks/non-incremental/QF_LIA/check"
+    os.mkdir("./generated")
     for filename in os.listdir(directory):
         smtFile = open(os.path.join(directory, filename), "r")
         script = parser.get_script(smtFile)
         formula = script.get_last_formula()
+        satisfiability = next(cmd.args[1] for cmd in script.filter_by_command_name([commands.SET_INFO]) if cmd.args[0] == ":status")
+        print(satisfiability)
+        script.to_file("./generated/" + filename + "_gen")
+        result = subprocess.run(['z3', '-file:' + os.path.join("generated", filename + "_gen")], stdout=subprocess.PIPE)
+        print(result.stdout.decode('utf-8'))
+        #TODO: add deletion of the directory
+
+
+
         #do transformations & Solving
 
+
     #TESTING
+    """
     smtFile = open("./benchmarks/non-incremental/LIA/psyco/017.smt2", "r")
     script = parser.get_script(smtFile)
     #most smt-lib formulas of the benchmarks have a bunch of assert statements which are all AND-ed by this
     formula = script.get_last_formula()
     print(formula)
+    """
 
 
 
